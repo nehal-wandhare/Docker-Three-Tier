9fe73c90b89b7512d79c4cbdb6441b065d4c211b
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-04-10 16:15:45 +0530

    Update AppTest.java

--- Backend/src/test/java/com/example/AppTest.java
+++ Backend/src/test/java/com/example/AppTest.java
@@ -6,26 +6,38 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppTest {
 
     @Test
-    public void testAddPositive() {
+    public void testAddPositiveNumbers() {
         App app = new App();
         assertEquals(5, app.add(2, 3));
     }
 
     @Test
-    public void testAddNegative() {
+    public void testAddNegativeNumbers() {
+        App app = new App();
+        assertEquals(-5, app.add(-2, -3));
+    }
+
+    @Test
+    public void testAddMixedNumbers() {
         App app = new App();
         assertEquals(-1, app.add(-2, 1));
     }
 
     @Test
-    public void testAddZero() {
+    public void testAddWithZero() {
         App app = new App();
         assertEquals(2, app.add(2, 0));
     }
 
     @Test
-    public void testAddBothNegative() {
+    public void testAddBothZero() {
         App app = new App();
-        assertEquals(-5, app.add(-2, -3));
+        assertEquals(0, app.add(0, 0));
+    }
+
+    @Test
+    public void testAddLargeNumbers() {
+        App app = new App();
+        assertEquals(1000000, app.add(500000, 500000));
     }
 }
