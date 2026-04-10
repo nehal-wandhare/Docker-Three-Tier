f448a1271a7d30e0f3c3cd01a2440c511c5895a4
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-04-10 16:05:44 +0530

    Update AppTest.java

--- Backend/src/test/java/com/example/AppTest.java
+++ Backend/src/test/java/com/example/AppTest.java
@@ -6,8 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppTest {
 
     @Test
-    public void testAdd() {
+    public void testAddPositive() {
         App app = new App();
-        assertEquals(5, app.add(2,3));
+        assertEquals(5, app.add(2, 3));
+    }
+
+    @Test
+    public void testAddNegative() {
+        App app = new App();
+        assertEquals(-1, app.add(-2, 1));
+    }
+
+    @Test
+    public void testAddZero() {
+        App app = new App();
+        assertEquals(2, app.add(2, 0));
+    }
+
+    @Test
+    public void testAddBothNegative() {
+        App app = new App();
+        assertEquals(-5, app.add(-2, -3));
     }
 }
